79d0d75ec5414175f1a4990bd72dab0b548bb07a
nirajsah004 <sah.niraj2004@gmail.com>
2024-06-25 03:11:43 +0545

    Create Lecture19-CipherSchool-Niraj.cpp

---
+++ Lecture19-CipherSchool-Niraj.cpp
@@ -0,0 +1,15 @@
+/* in this lecture we discussed about how elements are stored in our computer memory
+they are stored in bits and bytes
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a;
+    long long int b;
+    cout<<"Size of int is: "<<sizeof(a)<<endl;
+    cout<<"Size of long long int is: "<<sizeof(b);
+    
+    return 0;
+}
