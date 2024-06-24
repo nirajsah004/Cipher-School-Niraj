1572f04a567b6227cbfabd2915d71e6f8118c455
nirajsah004 <sah.niraj2004@gmail.com>
2024-06-24 23:41:48 +0545

    Create Lecture4-CipherSchool-Niraj.cpp

---
+++ Lecture4-CipherSchool-Niraj.cpp
@@ -0,0 +1,14 @@
+//how to declare datatypes and perform some operations
+#include<iostream>
+using namespace std;
+int main(){
+	int a=3;
+	int b=5;
+	// need to find sum of two numbers by declaring two integers a,b
+	cout<<a+b<<endl<<"a+b"<<endl;
+	
+	cout<<a+b;
+	cout<<endl;
+	cout<<"a+b";
+	return 0;
+}
