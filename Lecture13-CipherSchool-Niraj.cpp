072cb0ba54700f98530c98db86d4cd0d96c08e9e
nirajsah004 <sah.niraj2004@gmail.com>
2024-06-25 01:42:31 +0545

    Create Lecture13-CipherSchool-Niraj.cpp

---
+++ Lecture13-CipherSchool-Niraj.cpp
@@ -0,0 +1,13 @@
+/*
+in this class we discussed about function and what are they used for?
+so function is basically a task that is performed.
+a function has a header and a body(contains the task to perform)
+
+
+Advantage of using function:
+--reuseablity   -->we can use a function again and agian this saves times
+--abstraction   -->hides the useless information from the user
+--divivde and conqure   -->divide the task in small parts and solve our problem
+--improve redability    -->helps to make code more cleaner and easily understandable
+
+*/
