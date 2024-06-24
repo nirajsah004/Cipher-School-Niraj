987ba9c5df4666163cbf177aa926f15447052314
nirajsah004 <sah.niraj2004@gmail.com>
2024-06-25 00:27:13 +0545

    Create Lecture10-CipherSchool-Niraj.cpp

---
+++ Lecture10-CipherSchool-Niraj.cpp
@@ -0,0 +1,24 @@
+// post increment and pre increment 
+#include <iostream>
+using namespace std;
+int main() {
+    int i=0;
+    int j=0;
+    int k=0;
+cout<<i<<j<<k<<endl;
+j=i++;
+cout<<i<<j<<k<<endl;
+k=++i;
+cout<<i<<j<<k;
+    return 0;
+}
+// for loop to print numbers between 0 to 100
+#include <iostream>
+using namespace std;
+int main() {
+    int i;
+    for(i=1;i<100;i++){
+        cout<<i<<" ";
+    }
+    return 0;
+}
