e6171d989969b7efd675b3f24df267ed1029ccea
nirajsah004 <sah.niraj2004@gmail.com>
2024-07-07 01:26:36 +0545

    Create Lecture 37-Cipher School-Niraj.cpp

---
+++ Lecture 37-Cipher School-Niraj.cpp
@@ -0,0 +1,14 @@
+
+/*Selection sort
+    in this we found the least element then we put it at swap with first position then 
+    after that we repeat the steps 
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+
+    return 0;
+}
