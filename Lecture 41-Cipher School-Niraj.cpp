62bb1601930ca896412f761a49737b70fda69c58
nirajsah004 <sah.niraj2004@gmail.com>
2024-07-07 01:32:06 +0545

    Create Lecture 41-Cipher School-Niraj.cpp

---
+++ Lecture 41-Cipher School-Niraj.cpp
@@ -0,0 +1,5 @@
+/*in this class we discussed about which algorithm we shoul use as there are a number of solution for one problem
+so this depends on th eproblem for example if we want to use insertion sort on a small data it might take a small
+amount of time but on the same time if used on a large data set that will take a while to execute as the time complexity
+of insertion sort is o(n^2) and this will increase the time because of data size that is why we need to analyze 
+problem befor applying a algorithm*/
