562f30925c3598bbf47cbc8c864a24cb74fd5374
nirajsah004 <sah.niraj2004@gmail.com>
2024-07-07 01:35:49 +0545

    Create Lecture 43-Cipher School-Niraj.cpp

---
+++ Lecture 43-Cipher School-Niraj.cpp
@@ -0,0 +1,10 @@
+/*
+
+we need to choose an algorithm that works best in worst case as we will find problems in worst case not in the 
+best case so we judge a algorithm based on worst case scenario 
+
+
+
+Time Complexity- is time taken by code to run the algorithm
+Asymptotic analysis-
+*/
