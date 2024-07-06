70c0a43b9d6dac18fb0be2bb56be61ef85a91a9d
nirajsah004 <sah.niraj2004@gmail.com>
2024-07-07 01:02:21 +0545

    Create Lecture 21-Cipher School-Niraj.cpp

---
+++ Lecture 21-Cipher School-Niraj.cpp
@@ -0,0 +1,13 @@
+/*in this class we discussed about pointer in cpp and what are they used for
+pointers are used to store variable address we can do this by using " * " operator
+in this lecture we also discussed aboout major diffrence between address and value
+*/
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a;
+    float b;
+    cout<<"Address of interger value is: "<<&a<<endl<<"Address of floating value is: "<<&b<<endl;
+    return 0;
+}
