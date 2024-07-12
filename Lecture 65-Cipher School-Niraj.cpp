85da6742ec5e9945098bbb02026bb41a306cc495
nirajsah004 <sah.niraj2004@gmail.com>
2024-07-12 21:42:12 +0545

    Create Lecture 65-Cipher School-Niraj.cpp

---
+++ Lecture 65-Cipher School-Niraj.cpp
@@ -0,0 +1,15 @@
+/* in this lecture we learned about quick sort and how we are going to use quick sort using recursion in 
+this sorting we will make our array in this way such that there is an element that will retain its position
+ throughout the sorting that element needs to be created first and that can be done by sorting the first element
+ in its best location where all elements to the right are bigger than that element and all of them elements to the
+ left are smaller element then we will start the sorting
+*/
+#include <iostream>
+using namespace std;
+
+int main(){
+
+
+
+    return 0;
+}
