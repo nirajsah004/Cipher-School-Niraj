58fee67f4d9c95fa3828e6e82aaad4fc0a266311
nirajsah004 <sah.niraj2004@gmail.com>
2024-07-12 21:11:39 +0545

    Create Lecture 44-Cipher School-Niraj.cpp

---
+++ Lecture 44-Cipher School-Niraj.cpp
@@ -0,0 +1,14 @@
+/* we saw in class that the diffrence between x and x^2 is very large as the size of x increases the time taken
+by the compiler to execute the code will increase a lot that in relation to x
+
+so we need a code with complexity in kx
+
+we need to ignore the constants (they dont matter in large data sets)
+we need to ignore the lower order terms(at larger scale they diminish)
+
+Asymptotic relationship is relation between two equations at infinity and this tends to be a large value
+
+BIG O NOTATION is asymtotic upper bound 
+BIG OMEGA is the aymptotic lower bound 
+BIG THETHA is the middle bound of two values
+*/
