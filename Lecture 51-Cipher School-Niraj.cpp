c74d21ea1ea907dbe1d878938403c04b35b9940c
nirajsah004 <sah.niraj2004@gmail.com>
2024-07-12 21:22:20 +0545

    Create Lecture 51-Cipher School-Niraj.cpp

---
+++ Lecture 51-Cipher School-Niraj.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+
+// class is an user defined data type
+class Student{
+    public:
+    string name;
+    int marks;
+
+
+
+
+};
+
+int main(){
+    Student a,b,c;
+        // a,b,c are objects of the class student
+
+
+    return 0;
+}
