845f62c8cd89b0806107fcccf46b979579aab65d
nirajsah004 <sah.niraj2004@gmail.com>
2024-07-12 21:26:19 +0545

    Create Lecture 54-Cipher School-Niraj.cpp

---
+++ Lecture 54-Cipher School-Niraj.cpp
@@ -0,0 +1,11 @@
+/*
+in this class we discussed about linked list and why we use it instead of an array the reason being that the array
+is not flexible if we want to insert and element at an index we need to swap all the following elements also
+same goes for deleting the elements in an array, we also cant insert an element after the size of array is full
+
+
+Linked list has a node that further has data and address of next element stored in it so linked list is basically
+collection of nodes.
+
+These nodes are in series and stores data and address of next element
+*/
