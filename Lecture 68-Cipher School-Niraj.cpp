09e478d34fa97f0c34cafb1da22c06c6884faa27
nirajsah004 <sah.niraj2004@gmail.com>
2024-07-12 21:45:13 +0545

    Create Lecture 68-Cipher School-Niraj.cpp

---
+++ Lecture 68-Cipher School-Niraj.cpp
@@ -0,0 +1,19 @@
+/* in this class we discussed about tree data structure and why we use tree in data structure we use them for 
+branching hte data base
+
+
+*/
+#include <iostream>
+using namespace std;
+
+class node{
+    public:
+    int data;
+    node* left;
+    node* right;
+};
+
+int main(){
+
+    return 0;
+}
