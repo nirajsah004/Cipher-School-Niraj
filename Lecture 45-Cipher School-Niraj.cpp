086c8479bac4aeb3f9c165e6323c6aed3e2f85a0
nirajsah004 <sah.niraj2004@gmail.com>
2024-07-12 21:14:10 +0545

    Create Lecture 45-Cipher School-Niraj.cpp

---
+++ Lecture 45-Cipher School-Niraj.cpp
@@ -0,0 +1,2 @@
+/*Greedy Algorithm - approaching a problem in a greedy way and tey make repeated best decisions it can't be used
+for every problem and also not the best solution for every problem as there are may soluton to one problem*/
