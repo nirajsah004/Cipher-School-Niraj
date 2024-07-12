9408601fdb2bbe038319cb7947f793a33a753325
nirajsah004 <sah.niraj2004@gmail.com>
2024-07-12 21:21:21 +0545

    Create Lecture 50-Cipher School-Niraj.cpp

---
+++ Lecture 50-Cipher School-Niraj.cpp
@@ -0,0 +1,14 @@
+/* in this class we discussed about class and objects why they are used and how??
+we store similar objects in a class so a class is group of objects which can have any data types
+
+class is a template 
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+
+    return 0;
+}
